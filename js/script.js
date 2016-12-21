a88d9a376d0179393ef61625abd154082572e0be
DangerPNZ <dangerpnz@mail.ru>
2016-12-21 18:22:00 +0400

    Пиксельперфект

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var menu_btn = document.querySelector(".page-header__menu-btn");
 
 var modal_open_link = document.querySelector(".link-btn__modal-open-link");
 var modal = document.querySelector(".modal-add");
+var modal_overlay = document.querySelector(".modal-add__overlay");
 
 if (mobile_menu.classList.contains("page-header__main-menu--nojs")) {
             mobile_menu.classList.remove("page-header__main-menu--nojs");
@@ -20,6 +21,13 @@ modal_open_link.addEventListener("click", function(event) {
   event.preventDefault();
   modal.classList.add("modal-add--show");
 });
+modal_overlay.addEventListener("click", function(event) {
+  event.preventDefault();
+  modal.classList.remove("modal-add--show");
+});
+
+
+
 
 
 
